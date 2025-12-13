10156247ec5979e544f7990100ecd4f53c2c7dce
5474IV <94454510+5474IV@users.noreply.github.com>
2025-12-13 23:59:35 +0100

    Update main.py CH3: Player L2: Draw Player

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
 import constants as c
 from logger import log_state
+from player import Player
 
 def main():
 	pygame.init()
 	clock = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+	player = Player(c.SCREEN_WIDTH / 2, y = c.SCREEN_HEIGHT / 2)
+	
 	while(True):
 		log_state()
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 		print(dt)

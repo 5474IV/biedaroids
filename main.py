e328631a188a1991f287a4666757c5dadcbf592e
5474IV <94454510+5474IV@users.noreply.github.com>
2025-12-13 21:40:42 +0100

    Update main.py FPS dt log

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import constants as c
 from logger import log_state
 
 def main():
-	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-	print(f"Screen width: {c.SCREEN_WIDTH}")
-	print(f"Screen height: {c.SCREEN_HEIGHT}")
 	pygame.init()
+	clock = pygame.time.Clock
+	dt = 0
 	screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 	while(True):
 		log_state()
@@ -15,6 +14,8 @@ def main():
 				return
 		screen.fill("black")
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
+		print(dt)
 
 if __name__ == "__main__":
     main()

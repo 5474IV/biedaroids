31a8bc9e1b580ac74a2f49693dc14864df7008ec
5474IV <n0w4k0wsk1k@gmail.com>
2025-12-14 01:05:13 +0100

    CH4: Asteroids L1: Asteroids FINAL

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
-import constants as c
+from constants import *
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 	pygame.init()
@@ -10,11 +12,14 @@ def main():
 	
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
 	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
 
-	screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
-	player = Player(c.SCREEN_WIDTH / 2, y = c.SCREEN_HEIGHT / 2)
-	
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+	asteroid_field = AsteroidField()
 
 	while(True):
 		log_state()

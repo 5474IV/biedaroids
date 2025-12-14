a0e29bc5ca645f73d3ad6da0a5be3ce99c67ee97
5474IV <n0w4k0wsk1k@gmail.com>
2025-12-14 02:02:28 +0100

    CH4: Asteroids L6: Splitting FINALE

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from logger import log_event
 from constants import *
 from circleshape import CircleShape
 
@@ -10,3 +12,11 @@ class Asteroid(CircleShape):
 		pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
 	def update(self, dt):
 		self.position += self.velocity * dt
+	def split(self):
+		self.kill()
+		if self.radius <= ASTEROID_MIN_RADIUS:
+			return
+		log_event("asteroid_split")
+		angle = random.uniform(20, 50)
+		Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS).velocity = self.velocity.rotate(angle) * 1.2
+		Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS).velocity = self.velocity.rotate(-angle) * 1.2
